77bc15f6d5c83a929fb538b9548f1dface429bf6
Gaskov Vadim <vadik.gaskoff@yandex.ru>
2024-11-27 16:33:55 +0800

    init

---
+++ BotConstructor.Domain/Entities/Section.cs
@@ -0,0 +1,9 @@
+﻿namespace BotConstructor.Domain.Entities;
+
+public class Section
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string? Message { get; set; }
+    public Section? ParentSection { get; set; }
+}
